034866301293b868dd93814c4698f884222c95b8
wly <1355498705@qq.com>
2019-06-27 01:40:28 +0800

    配置路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { DemoComponent } from './components/demo/demo.component';
 
 const routes: Routes = [
-  {path:'',component:ProductListComponent}
+  {path:'',component:ProductListComponent},
+  {path:'demo',component:DemoComponent}
 ];
 
 @NgModule({
