244c3772ec056e1b7595cc5ca61323bd7fadec51
wly <1355498705@qq.com>
2019-06-27 00:19:03 +0800

    creat components/top-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',//使用组件名称
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-demo';
+  title = 'angular-demo';//定义属性
+  constructor(){}//构造函数
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 /**根组件 */
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 /* @NgModule装饰器，接受一个元数据对象，告诉angular如何编译和启动应用*/
 @NgModule({
   //配置组件
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent
   ],
   //注入依赖的的其他模块
   imports: [
